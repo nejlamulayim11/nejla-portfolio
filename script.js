ba939c5ed7f21e5144bf4477ea5c8b7d60f162ec
nejlamulayim11 <ntysoftware@gmail.com>
2025-06-17 22:55:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Mesajınız gönderildi! En kısa sürede sizinle iletişime geçeceğim.");
+});
